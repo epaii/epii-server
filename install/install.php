86e4edacd30364c67c20bfb02bad8e247d0c5821
MrRen <543169072@qq.com>
2018-08-17 10:04:41 +0800

    edit install

--- install/install.php
+++ install/install.php
@@ -10,9 +10,7 @@ $base_root = str_replace(DIRECTORY_SEPARATOR . "install", "", __DIR__);
 $this_dir = __DIR__;
 $is_win = strtoupper(substr(PHP_OS, 0, 3)) == 'WIN';
 $shall_ext = $is_win ? "bat" : "sh";
-
-var_dump($_SERVER);
-var_dump($_ENV);
+$_SERVER['_'] = getPhp();
 $ini = parse_ini_file($base_root . DIRECTORY_SEPARATOR . "config.ini", true);
 $ini['server']['nginx_root'] = dirname($ini['nginx']['cmd']);
 $ini['server']['php_cmd'] = $_SERVER['_'];
@@ -24,8 +22,8 @@ if (!isset($ini['server']['domain_other'])) {
 if (!isset($ini['server']['www_dir'])) $ini['server']['www_dir'] = $base_root . DIRECTORY_SEPARATOR . "web";
 
 
-$find = ["domain_app","base_root", "this_ip", "this_port", "php_root", "nginx_root", "domain_this", "domain_other", "domain_this_1", "domain_other_1", "www_dir", "nginx_cmd", "php_cmd"];
-$replace = [implode(" ",array_keys($ini['domain_app'])),$base_root, $ini['server']['this_ip'], $ini['server']['this_port'], $ini['php']['root'][0], $ini['server']['nginx_root'], $ini['server']['domain_this'], $ini['server']['domain_other'], str_replace(".", "\\.", $ini['server']['domain_this']), str_replace(".", "\\.", $ini['server']['domain_other']), isset($ini['server']['www_dir']) ? $ini['server']['www_dir'] : $base_root . DIRECTORY_SEPARATOR . "web", $ini['nginx']['cmd'], $ini['server']['php_cmd']];
+$find = ["domain_app", "base_root", "this_ip", "this_port", "php_root", "nginx_root", "domain_this", "domain_other", "domain_this_1", "domain_other_1", "www_dir", "nginx_cmd", "php_cmd"];
+$replace = [implode(" ", array_keys($ini['domain_app'])), $base_root, $ini['server']['this_ip'], $ini['server']['this_port'], $ini['php']['root'][0], $ini['server']['nginx_root'], $ini['server']['domain_this'], $ini['server']['domain_other'], str_replace(".", "\\.", $ini['server']['domain_this']), str_replace(".", "\\.", $ini['server']['domain_other']), isset($ini['server']['www_dir']) ? $ini['server']['www_dir'] : $base_root . DIRECTORY_SEPARATOR . "web", $ini['nginx']['cmd'], $ini['server']['php_cmd']];
 
 $root_dir = "";
 foreach ($ini['root_dir'] as $key => $value) {
@@ -67,15 +65,13 @@ if ($domian_when_ip) {
 }
 
 
-
 $domain_app_list = "";
 foreach ($ini['domain_app'] as $key => $value) {
-    $appinfo = explode("/",$value);
-    for ($i =0;$i<4;$i++)
-    {
+    $appinfo = explode("/", $value);
+    for ($i = 0; $i < 4; $i++) {
         if (!$appinfo[$i]) $appinfo[$i] = -1;
     }
-    $domain_app_list .= parse_tpl($this_dir . DIRECTORY_SEPARATOR . "tpls" . DIRECTORY_SEPARATOR . "nginx_domain_app.tpl", [ "domain", "app","app_sub","app_sub2","app_sub3"], [$key,$appinfo[0],$appinfo[1],$appinfo[2],$appinfo[3]]);
+    $domain_app_list .= parse_tpl($this_dir . DIRECTORY_SEPARATOR . "tpls" . DIRECTORY_SEPARATOR . "nginx_domain_app.tpl", ["domain", "app", "app_sub", "app_sub2", "app_sub3"], [$key, $appinfo[0], $appinfo[1], $appinfo[2], $appinfo[3]]);
 }
 
 $find[] = "domain_app_list";
@@ -84,9 +80,6 @@ $replace[] = $domain_app_list;
 
 $nignx_config_tpl = $this_dir . DIRECTORY_SEPARATOR . "tpls" . DIRECTORY_SEPARATOR . 'ws.conf.tpl';
 $nignx_config = $base_root . DIRECTORY_SEPARATOR . "configs" . DIRECTORY_SEPARATOR . "nginx" . DIRECTORY_SEPARATOR . 'ws.conf';
-
-
-
 
 
 parse_tpl($nignx_config_tpl, $find, $replace, $nignx_config);
@@ -108,17 +101,11 @@ file_put_contents($nignx_config_root, $myconfig . "\r\n" . $pd . "\r\n}");
 
 $php_bat = "";
 foreach ($ini['php']['php_cgi'] as $key => $value) {
-    $php_bat .= parse_tpl($this_dir . DIRECTORY_SEPARATOR . "tpls" . DIRECTORY_SEPARATOR . "start.php.tpl", ["base_root","i", "port", "root", "cmd"], [$base_root,$key, $ini['php']['port'][$key], dirname($value), $value]);
+    $php_bat .= parse_tpl($this_dir . DIRECTORY_SEPARATOR . "tpls" . DIRECTORY_SEPARATOR . "start.php.tpl", ["base_root", "i", "port", "root", "cmd"], [$base_root, $key, $ini['php']['port'][$key], dirname($value), $value]);
 }
 
 $find[] = "php_bat";
 $replace[] = $php_bat;
-
-
-
-
-
-
 
 
 $start_tpl = $this_dir . DIRECTORY_SEPARATOR . "tpls" . DIRECTORY_SEPARATOR . 'start.cmd.php.tpl';
@@ -127,29 +114,28 @@ parse_tpl($start_tpl, $find, $replace, $start_bat);
 
 
 $start_tpl = $this_dir . DIRECTORY_SEPARATOR . "tpls" . DIRECTORY_SEPARATOR . 'start.bat.tpl';
-$start_bat = $base_root . DIRECTORY_SEPARATOR . "start.".$shall_ext;
+$start_bat = $base_root . DIRECTORY_SEPARATOR . "start." . $shall_ext;
 parse_tpl($start_tpl, ['php_cmd'], [$ini['server']['php_cmd']], $start_bat);
 
 
-$stop_tpl = $this_dir . DIRECTORY_SEPARATOR . "tpls" . DIRECTORY_SEPARATOR . 'stop.'.$shall_ext.'.tpl';
-$staop_bat = $base_root . DIRECTORY_SEPARATOR . "stop.".$shall_ext;
+$stop_tpl = $this_dir . DIRECTORY_SEPARATOR . "tpls" . DIRECTORY_SEPARATOR . 'stop.' . $shall_ext . '.tpl';
+$staop_bat = $base_root . DIRECTORY_SEPARATOR . "stop." . $shall_ext;
 parse_tpl($stop_tpl, $find, $replace, $staop_bat);
 
 //$re_install_tpl = $this_dir . DIRECTORY_SEPARATOR . "tpls" . DIRECTORY_SEPARATOR . 're_install.'.$shall_ext.'.tpl';
 //$re_install_bat = $base_root . DIRECTORY_SEPARATOR . "re_install.".$shall_ext;
 //parse_tpl($re_install_tpl, $find, $replace, $re_install_bat);
 
-if (!$is_win)
-{
-   // chmod($re_install_bat,777);
-   chmod($staop_bat,0777);
-   chmod($start_bat,0777);
-   chmod($nignx_config,0777);
+if (!$is_win) {
+    // chmod($re_install_bat,777);
+    chmod($staop_bat, 0777);
+    chmod($start_bat, 0777);
+    chmod($nignx_config, 0777);
 
 }
 
-$lock_file = __DIR__.DIRECTORY_SEPARATOR.".time";
-file_put_contents($lock_file,filectime($lock_file));
+$lock_file = __DIR__ . DIRECTORY_SEPARATOR . ".time";
+file_put_contents($lock_file, filectime($lock_file));
 
 foreach (["web", "logs"] as $d) {
     $web_dir = $base_root . DIRECTORY_SEPARATOR . $d;
@@ -179,4 +165,33 @@ function parse_tpl($tpl_file, $find, $replace, $to_file = null)
         return $txt;
     }
 
+}
+
+
+function getPhp()
+{
+    if (substr(strtolower(PHP_OS), 0, 3) == 'win') {
+
+        $ini = ini_get_all();
+
+        $path = $ini['extension_dir']['local_value'];
+
+        $php_path = str_replace('\\', '/', $path);
+
+        $php_path = str_replace(array('/ext/', '/ext'), array('/', '/'), $php_path);
+
+        $real_path = $php_path . 'php.exe';
+
+    } else {
+
+        $real_path = PHP_BINDIR . '/php';
+
+    }
+
+    if (strpos($real_path, 'ephp.exe') !== FALSE) {
+
+        $real_path = str_replace('ephp.exe', 'php.exe', $real_path);
+
+    }
+    return $real_path;
 }
